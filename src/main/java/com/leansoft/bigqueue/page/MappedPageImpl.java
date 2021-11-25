e20009c0e8b62a7c7ca78db503fbe722dab6f98e
Nitish Goyal <nitish.goyal@phonepe.com>
2021-11-25 18:31:07 +0530

    Revert code to original in java 8 version

--- src/main/java/com/leansoft/bigqueue/page/MappedPageImpl.java
+++ src/main/java/com/leansoft/bigqueue/page/MappedPageImpl.java
@@ -1,14 +1,13 @@
 package com.leansoft.bigqueue.page;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import sun.misc.Unsafe;
-
 import java.io.Closeable;
 import java.io.IOException;
-import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MappedPageImpl implements IMappedPage, Closeable {
 	
@@ -87,26 +86,36 @@ public class MappedPageImpl implements IMappedPage, Closeable {
      * Helper class allowing to clean direct buffers.
      */
     private static class Cleaner {
-
-        private static Unsafe unsafe;
+        public static final boolean CLEAN_SUPPORTED;
+        private static final Method directBufferCleaner;
+        private static final Method directBufferCleanerClean;
 
         static {
+            Method directBufferCleanerX = null;
+            Method directBufferCleanerCleanX = null;
+            boolean v;
             try {
-                Field f = Unsafe.class.getDeclaredField("theUnsafe");
-                f.setAccessible(true);
-                unsafe = (Unsafe) f.get(null);
+                directBufferCleanerX = Class.forName("java.nio.DirectByteBuffer").getMethod("cleaner");
+                directBufferCleanerX.setAccessible(true);
+                directBufferCleanerCleanX = Class.forName("sun.misc.Cleaner").getMethod("clean");
+                directBufferCleanerCleanX.setAccessible(true);
+                v = true;
             } catch (Exception e) {
-                logger.warn("Unsafe Not support {}", e.getMessage(), e);
+                v = false;
             }
+            CLEAN_SUPPORTED = v;
+            directBufferCleaner = directBufferCleanerX;
+            directBufferCleanerClean = directBufferCleanerCleanX;
         }
 
         public static void clean(ByteBuffer buffer) {
     		if (buffer == null) return;
-            if (buffer.isDirect()) {
-                if (unsafe != null) {
-                    unsafe.invokeCleaner(buffer);
-                } else {
-                    logger.warn("Unable to clean bytebuffer");
+            if (CLEAN_SUPPORTED && buffer.isDirect()) {
+                try {
+                    Object cleaner = directBufferCleaner.invoke(buffer);
+                    directBufferCleanerClean.invoke(cleaner);
+                } catch (Exception e) {
+                    // silently ignore exception
                 }
             }
         }
